faf0e4ac5e06cbd7216fbd07ba830261e0862769
Herson Marroquin <hersonmarroquin30@gmail.com>
2022-06-05 07:32:36 -0700

    done with ui/ux css

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,11 +1,26 @@
+import { FaTwitterSquare, FaShareSquare, FaQuoteLeft } from 'react-icons/fa';
+
 function QuoteBox() {
   return (
     <main>
       <div className='main-container'>
         <div className='quote-box-container'>
-          <p className='paragraph'>
-            A house divided against itself cannot stand.
-          </p>
+          <div className='quote'>
+            <p className='paragraph'>
+              <span>
+                <FaQuoteLeft className='quote-logo' />
+              </span>
+              A house divided against itself cannot stand.
+            </p>
+            <p className='author-name'>- Yanib Gonon</p>
+          </div>
+          <div className='share-new'>
+            <div className='share-quote'>
+              <FaTwitterSquare className='share-logo' />
+              <FaShareSquare className='share-logo' />
+            </div>
+            <button className='new-quote'>New Quote</button>
+          </div>
         </div>
       </div>
     </main>
