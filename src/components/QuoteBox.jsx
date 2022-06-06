58c907dd2aa71d28f6cd95d4962f08cd4d110d7e
Herson Marroquin <hersonmarroquin30@gmail.com>
2022-06-06 07:17:47 -0700

    created everything, but the change color animation

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,6 +1,32 @@
 import { FaTwitterSquare, FaShareSquare, FaQuoteLeft } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
 
 function QuoteBox() {
+  const API_URL = 'https://type.fit/api/quotes';
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const fetchQuotes = async () => {
+    try {
+      const response = await fetch(API_URL);
+      if (!response.ok) throw Error('Did not receive expected data.');
+      const newQuote = await response.json();
+      const randNum = Math.floor(Math.random() * newQuote.length) + 1;
+      setQuote(newQuote[randNum].text);
+      setAuthor(newQuote[randNum].author);
+    } catch (err) {
+      console.log(err.stack);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+
+  const handleClick = () => {
+    fetchQuotes();
+  };
+
   return (
     <main>
       <div className='main-container'>
@@ -10,16 +36,29 @@ function QuoteBox() {
               <span>
                 <FaQuoteLeft className='quote-logo' />
               </span>
-              A house divided against itself cannot stand.
+              {quote}
             </p>
-            <p className='author-name'>- Yanib Gonon</p>
+            <p className='author-name'>
+              - {author === null ? 'Yanib Gonon' : author}
+            </p>
           </div>
           <div className='share-new'>
             <div className='share-quote'>
-              <FaTwitterSquare className='share-logo' />
-              <FaShareSquare className='share-logo' />
+              <a
+                href={`https://twitter.com/intent/tweet?text="${quote}"%20-%20${author}.`}
+                target='_blank'
+                rel='noreferrer'
+              >
+                <FaTwitterSquare className='share-logo' />
+              </a>
+              <FaShareSquare
+                className='share-logo'
+                onClick={() => navigator.clipboard.writeText(quote)}
+              />
             </div>
-            <button className='new-quote'>New Quote</button>
+            <button className='new-quote' onClick={handleClick}>
+              New Quote
+            </button>
           </div>
         </div>
       </div>
